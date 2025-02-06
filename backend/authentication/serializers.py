99c251a3a56c9a90504e2d1814a1e3bc64ff533d
Aleksander Pres <aleksander.pres98@gmail.com>
2025-02-06 17:18:05 +0100

    Safe password validation added

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 from .models import User
 
 
@@ -10,9 +10,31 @@ class RegistrationSerializer(ModelSerializer):
     
     def create(self, validation_data):
         password = validation_data.pop('password')
+
+        # Check if password is safe
+        contains_digit = False
+        contains_special_character = False
+
+        special_characters = [
+            "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+            "[", "]", "{", "}", "\\", "|", ";", ":", "'", '"', ",", "<", ".", ">", "/", "?"
+        ]
+        digits = [str(digit) for digit in range(10)]
+
+        for character in password:
+            if character in special_characters:
+                contains_special_character = True
+            elif character in digits:
+                contains_digit = True
+
+        if len(password) < 12:
+            raise ValidationError("Password must be at least 12 characters long.")
+        if not contains_digit:
+            raise ValidationError("Password must contain at least one digit.")
+        if not contains_special_character:
+            raise ValidationError("Password must contain at least one special character.")
+        
         user = User.objects.create(**validation_data)
         user.set_password(password)
         user.save()
         return user
-    
-
