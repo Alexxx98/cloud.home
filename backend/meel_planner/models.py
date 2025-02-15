ee38bd633700d48a66aa1c85e07a111c07785317
Aleksander Pres <aleksander.pres98@gmail.com>
2025-02-15 11:34:54 +0100

    meal_planner models created

--- backend/meel_planner/models.py
+++ backend/meel_planner/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    ingredient_type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.ingredient_type}, {self.name}'
+
+
+class Recipe(models.Model):
+    name = models.CharField()
+    ingredients = ArrayField(
+        models.ForeignKey(
+            Ingredient,
+            on_delete=models.CASCADE,
+        ), 
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}'
+    
+
+class MealPlan(models.Model):
+    name=models.CharField(max_length=64)
+    recipes = ArrayField(
+        models.ForeignKey(
+            Recipe,
+            on_delete=models.CASCADE
+        ),
+        size=7
+    )
+
+    def __str__(self):
+        return f'{self.name}'
