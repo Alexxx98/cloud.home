87c9b4a4146cc046df4af711d423997d61f36878
Aleksander Pres <aleksander.pres98@gmail.com>
2025-02-17 13:19:41 +0100

    few changes in meal_planner models

--- backend/meal_planner/models.py
+++ backend/meal_planner/models.py
@@ -5,6 +5,10 @@ from django.contrib.postgres.fields import ArrayField
 class Ingredient(models.Model):
     name = models.CharField(max_length=64, unique=True)
     ingredient_type = models.CharField(max_length=64)
+    calories = models.IntegerField(default=0)
+    protein  = models.DecimalField(defualt=0.0, decimal_places=1)
+    carbohydrate = models.DecimalField(default=0.0, decimal_places=1)
+    fat = models.DecimalField(default=0.0, decimal_places=1)
 
     def __str__(self):
         return f'{self.name}'
@@ -16,6 +20,7 @@ class Recipe(models.Model):
         Ingredient,
         related_name='ingredients'
     )
+    servings = models.IntegerField(default=1)
     content = models.TextField()
 
     def __str__(self):
